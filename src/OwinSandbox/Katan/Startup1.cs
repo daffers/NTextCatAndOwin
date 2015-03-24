15b4d2f607f92f6f7414d5a31cf74d63ba4e775a
Dafydd Giddins <dafydd.giddins@live.co.uk>
2015-03-24 22:59:19 +0000

    Adding experimental projects

---
+++ src/OwinSandbox/Katan/Startup1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using Microsoft.Owin;
+using Newtonsoft.Json;
+using Owin;
+
+[assembly: OwinStartup(typeof(Katan.Startup1))]
+
+namespace Katan
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.Run(context =>
+            {
+                string postedText = "empty";
+
+                if (context.Request.Method.ToLowerInvariant() == "POST".ToLowerInvariant())
+                {
+                    var reader = new StreamReader(context.Request.Body);
+                    var whatWasRead = reader.ReadToEnd();
+                    var input = JsonConvert.DeserializeObject<LanguageToDetect>(whatWasRead);
+                    postedText = input.TextToTranslate;
+                }
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("text to translate: " + postedText);
+            });
+
+
+        }
+    }
+
+    public class LanguageToDetect
+    {
+        [JsonProperty(PropertyName = "profileToUse")]
+        public string ProfileToUse { get; set; }
+
+        [JsonProperty(PropertyName = "textToTranslate")]
+        public string TextToTranslate { get; set; }
+    }
+}
---
+++ src/OwinSandbox/Nancy/NTextCatModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.ModelBinding;
+using Newtonsoft.Json;
+
+namespace NancyTest
+{
+    public class NTextCatModule : Nancy.NancyModule   
+    {
+        public NTextCatModule()
+        {
+            Get["/"] = _ => "hello";
+            Post["/detect"] = request =>
+            {
+                var input = this.Bind<LanguageToDetect>();
+                return input.TextToTranslate;
+                
+            };
+        }
+    }
+
+
+
+    public class LanguageToDetect
+    {
+        [JsonProperty(PropertyName = "profileToUse")]
+        public string ProfileToUse { get; set; }
+
+        [JsonProperty(PropertyName = "textToTranslate")]
+        public string TextToTranslate { get; set; }
+    }
+}
